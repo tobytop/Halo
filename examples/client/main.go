c896606949437c85729216466bad94d72de3c744
tobytop <homelea@163.com>
2022-08-30 15:35:48 +0800

    fix a bit

--- examples/client/main.go
+++ examples/client/main.go
@@ -7,17 +7,15 @@ import (
 )
 
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	client := halo.NewClient(ctx, "127.0.0.1:8000", func() map[string]halo.JobHandler {
+	client := halo.NewClient(context.Background(), "127.0.0.1:8000", func() map[string]halo.JobHandler {
 		handlers := make(map[string]halo.JobHandler)
 		handlers["test"] = &TestTask{
 			Name: "tsgggggg",
 		}
 		return handlers
 	}, 1)
-	client.StartServer()
 	defer client.StopServer()
+	client.StartServer()
 }
 
 type TestTask struct {
